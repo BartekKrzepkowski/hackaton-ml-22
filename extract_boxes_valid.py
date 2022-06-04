6cfee15cb7951c0832498c2dde2bc9c4740c3806
Vladyslav Kyryk <vkyryk@deloittece.com>
2022-06-04 21:08:34 +0200

    added image border padding before resizing

--- extract_boxes_valid.py
+++ extract_boxes_valid.py
@@ -14,6 +14,10 @@ if not os.path.isdir(new_valid_dir):
 with open(valid_metadata_json) as f:
     valid_meta = json.load(f)
 
+categories = {}
+for cat in valid_meta['categories']:
+    categories[cat['id']] = cat['name']
+
 new_img_shape = (224, 224)
 
 for annotation in valid_meta['annotations']:
@@ -25,15 +29,23 @@ for annotation in valid_meta['annotations']:
         if img_data['id'] == annotation['image_id']:
             break
     img_file = img_data['file_name']
-            
-    # find category by id
-    for cat in valid_meta['categories']:
-        if cat['id'] == annotation['category_id']:
-            break
-    category = cat['name']
+
+    category = categories[annotation['category_id']]
     
     img = cv2.imread(f'{valid_dir}{img_file}', 1)
     bbox_img = img[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+
+    max_dim_size = max(bbox[2], bbox[3])
+    x_dim_shortage, y_dim_shortage = max_dim_size - bbox[2], max_dim_size - bbox[3]
+    img_square = cv2.copyMakeBorder(
+        bbox_img,
+        top=int(y_dim_shortage / 2),
+        bottom=int(y_dim_shortage / 2),
+        left=int(x_dim_shortage / 2),
+        right=int(x_dim_shortage / 2),
+        borderType=cv2.BORDER_CONSTANT,
+        value=[82, 82, 82]
+    )
     
     resized = cv2.resize(bbox_img, new_img_shape)
 
